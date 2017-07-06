608ad3b5921aa0502ec27b23d5b44961a9410095
carloseduardov8 <carloseduardov8@hotmail.com>
2017-07-06 19:58:52 -0300

    Deeply comment Problem A of ACM 2016

--- ACM_2016/Problem_A_Assigning_Teams/program.cpp
+++ ACM_2016/Problem_A_Assigning_Teams/program.cpp
@@ -16,11 +16,15 @@ int main(){
 
     //Program:
     int min=10000, calc;
+
+    //Brute forces every combination of teams:
     do {
         calc = abs(weights[0] + weights[1] - (weights[2] + weights[3]));
         if (calc < min){
             min = calc;
         }
     } while (next_permutation(weights.begin(), weights.end()));
+
+    //Prints result:
     cout << min << endl;;
 }
