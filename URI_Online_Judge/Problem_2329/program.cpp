6fb4e87fa4a13fe42ecc4deb5dcfeadd88faac99
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-05-01 01:09:51 -0300

    Continue attempt to solve Problem 2329 of URI

--- URI_Online_Judge/Problem_2329/program.cpp
+++ URI_Online_Judge/Problem_2329/program.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int
-fatiar(int* arr, int k, int fat){
+int fatiar(int* arr, int k, int fat){
     int acc=0;
     for (int i=0; i<k; i++){
         acc += arr[i]/fat;
@@ -12,17 +11,16 @@ fatiar(int* arr, int k, int fat){
     return acc;
 }
 
-int
-main(){
+int main(){
     int n; cin >> n; // (10)
     int k; cin >> k;
-    int p[k];
+    int arr[k];
 
     long long int s = 0; // soma total; (616)
 
     for(int i=0; i<k; i++){
-        cin >> p[i];
-        s += p[i];
+        cin >> arr[i];
+        s += arr[i];
     }
 
     if (s < n){
@@ -30,29 +28,40 @@ main(){
         return 0;
     }
 
-    int _max = s/n; // limite superior (61)
-    int _min = 1;   // limite inferior (1)
+    int _max = (s/n) + 1;
+    int _min = 0;
+    //cout << "Limite Superior: " << _max <<endl;
+    //cout << "Limite Inferior: " << _min <<endl;
 
-    cout << "Limite Superior: " << _max <<endl;
-    cout << "Limite Inferior: " << _min <<endl;
-
-    int t;
+    int cut;
     int ps;
+	int bestResult = 0;
 
     do {
-        t  = (_max + _min)/2;
-        ps = fatiar(p, k, t);
-        cout << "Pessoas Servidas: " << ps;
-        cout << " onde cada fatia tem " << t << " metros." << endl;
 
-        if (ps < n)
-            _max = t; //(58)
-        else
-            _min = t; //(57)
+        cut  = (_max + _min)/2;
+        ps = fatiar(arr, k, cut);
+        //cout << "Pessoas Servidas: " << ps;
+        //cout << " onde cada cut tem " << cut << " metros." << endl;
 
-        cout << "max = " << _max << endl << "min = " << _min << endl;
+		// Case where cut was OK but might be possible to still become larger:
+		if (ps >= n) {
+			// Checks if result is current best:
+			if (cut > bestResult){
+				bestResult = cut;
+			}
+			// Tries to go for a larger cut:
+			_min = cut + 1;
+		// Case where cut was too big and did not serve all people:
+		} else {
+			// Makes next cut smaller:
+            _max = cut;
+		}
+
+        //cout << "max = " << _max << endl << "min = " << _min << endl;
     } while (_max > _min);
 
-    cout << t << endl;
-return 0;
+
+    cout << bestResult << endl;
+	return 0;
 }
